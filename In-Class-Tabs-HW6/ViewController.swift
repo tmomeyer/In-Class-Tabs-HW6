c746b8630f802c7f0428dc0830041a7f6d5eee02
Trevor Momeyer <t.momeyer@setonhill.edu>
2020-03-10 11:45:40 -0400

    Initial Commit

---
+++ In-Class-Tabs-HW6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  In-Class-Tabs-HW6
+//
+//  Created by Trevor Momeyer on 3/10/20.
+//  Copyright © 2020 Trevor Momeyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
